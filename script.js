677933286df996baf0d3af40faf665695a51680a
Kajetan Kasperuk <kajetankasperuk@gmail.com>
2018-06-15 19:26:07 +0200

    working on the controls

--- script.js
+++ script.js
@@ -1,49 +1,66 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
-var snakeX = 0;
-var snakeY = 0;
+var snake;
+// var snakeX = 0;
+// var snakeY = 0;
 var dy = 0;
 var dx = 0;
-var snakeWidth = 20;
-var snakeHeight = 20;
-var appleWidth = 20;
-var appleHeight = 20;
+var tileSize = 10;
+// var snakeWidth = 20;
+// var snakeHeight = 20;
+// var appleWidth = 20;
+// var appleHeight = 20;
 var rightPressed = false;
 var leftPressed = false;
 var upPressed = false;
 var downPressed = false;
-var appleX = (Math.floor(Math.random()*10)) * appleWidth;
-var appleY = (Math.floor(Math.random()*10)) * appleWidth;
+var direction;
+// var appleX = (Math.floor(Math.random()*10)) * tileSize;
+// var appleY = (Math.floor(Math.random()*10)) * tileSize;
 
 
 
+function snakeLength() {
+    var length = 4;
+    snake = [];
+    for (var i = length-1; i>=0; i--) {
+        snake.push({x:i, y:0});
+    }
+}
 
-function drawSnake() {
+
+function drawSnake(x, y) {
     ctx.beginPath();
-    ctx.rect(snakeX, snakeY, snakeWidth, snakeHeight);
+    ctx.rect(x*tileSize, y*tileSize, tileSize, tileSize);
     ctx.fillStyle = "#dd1817";
     ctx.fill();
     ctx.closePath();
 }
 
-function drawApple() {
-    ctx.beginPath();
-    ctx.rect(appleX, appleY, appleWidth, appleHeight);
-    ctx.fillStyle = "#3a3fdd";
-    ctx.fill();
-    ctx.closePath();
-}
+
+
+// function drawApple() {
+//     ctx.beginPath();
+//     ctx.rect(appleX, appleY, tileSize, tileSize);
+//     ctx.fillStyle = "#3a3fdd";
+//     ctx.fill();
+//     ctx.closePath();
+// }
 
 document.addEventListener("keydown", keyDownHandler, false);
 function keyDownHandler(e) {
     if (e.keyCode === 39) {
         rightPressed = true;
+        direction = 'right';
     } else if (e.keyCode === 37) {
         leftPressed = true;
+        direction = 'left';
     } else if (e.keyCode === 38) {
         upPressed = true;
+        direction = 'up';
     } else if (e.keyCode === 40) {
         downPressed = true;
+        direction = 'down';
     }
 }
 document.addEventListener("keyup", keyUpHandler, false);
@@ -61,19 +78,21 @@ function keyUpHandler(e) {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawSnake();
-    drawApple();
-    if (rightPressed) {
-        snakeX += appleWidth;
-    } else if (leftPressed) {
-        snakeX -= appleWidth;
-    } else if (upPressed) {
-        snakeY -= appleWidth;
-    } else if (downPressed) {
-        snakeY += appleWidth;
+    // drawApple();
+    snakeLength();
+    var snakeX = snake[0].x;
+    var snakeY = snake[0].y;
+    for (var i=0; i<snake.length; i++) {
+        drawSnake(snake[i].x, snake[i].y)
     }
-    if (snakeX === appleX && snakeY === appleY) {
-        snakeWidth += 20;
+    if (rightPressed) {
+        snakeX += tileSize;
+    } else if (leftPressed) {
+        snakeX -= tileSize;
+    } else if (upPressed) {
+        snakeY -= tileSize;
+    } else if (downPressed) {
+        snakeY += tileSize;
     }
 
 
